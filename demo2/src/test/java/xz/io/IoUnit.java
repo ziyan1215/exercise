f07e02db378d12059a7bff0203efbfa50be4e378
xuziyan <xuziyanmiss@gmail.com>
2018-11-30 16:21:24 +0800

    2018年11月30日 16:21:22

---
+++ demo2/src/test/java/xz/io/IoUnit.java
@@ -0,0 +1,44 @@
+package xz.io;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+
+public class IoUnit {
+	
+	
+
+
+	public static void main(String[] args) throws IOException {
+		/*创建可以读取文本文件的流对象，FileReader 让创建好的流对象和指定的文件相关联。
+		*/
+		FileReader fr = new FileReader("demo.txt");
+		int ch = 0;
+		while((ch = fr.read())!= -1) { //条件是没有读到结尾
+		System.out.println((char)ch); //调用读取流的 read 方法，读取一个字符。
+		}
+		fr.close();
+		
+	}
+
+	/*public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		1：创建一个字符输出流对象，用于操作文件。该对象一建立，就必须明确数据存储位置，是一个文件。
+		2：对象产生后，会在堆内存中有一个实体，同时也调用了系统底层资源，在指定的位置创建了一个存储数据的文件。
+		3：如果指定位置，出现了同名文件，文件会被覆盖。
+		
+		FileWriter fw = new FileWriter("A2.txt",true); // FileNotFoundException
+		
+		调用Writer类中的write方法写入字符串。字符串并未直接写入到目的地中，而是写入到了流中，(其实是写入到内存缓冲区中)。怎么把数据弄到文件中？
+		
+		fw.write("abcdea");
+		fw.flush(); // 刷新缓冲区，将缓冲区中的数据刷到目的地文件中。
+		fw.close(); // 关闭流，其实关闭的就是java调用的系统底层资源。在关闭前，会先刷新该流。
+	
+	}
+*/
+}
---
+++ demo2/src/test/java/xz/io/IoUnitTest.java
@@ -0,0 +1,133 @@
+package xz.io;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import org.junit.Test;
+
+
+/**
+ * @author Administrator LAST_IPDATE2018年11月30日上午11:21:59
+ */
+/**
+ * @author Administrator
+ *LAST_IPDATE2018年11月30日上午11:30:48
+ */
+public class IoUnitTest {
+
+	@Test
+	public void test() {
+		String s = "22";// 将字符串出转换成16进制表示
+		int i;
+		int j;
+		for (i = 0; i < s.length(); i++) {
+			j = s.charAt(i);
+			System.out.println(Integer.toHexString(j));
+		}
+	}
+
+	/*
+	 * IO异常处理
+	 */
+	@Test
+	public void f() {
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter("demo.txt", true);
+			fw.write("abcde");
+		} catch (IOException e) {
+			System.out.println(e.toString() + "....");
+		} finally {
+			if (fw != null)
+				try {
+					fw.close();
+				} catch (IOException e) {
+					System.out.println("close:" + e.toString());
+				}
+		}
+	}
+
+	@Test // 自定义缓冲区来读取数据
+	// 1、获取流对象
+	// 2、创建缓存数组
+	// 3.使用 read(char[])方法，将读取到字符存入数组
+	public void f1() throws IOException {
+		FileReader fr = new FileReader("demo.txt"); // 创建读取流对象和指定文件关联。
+		// 因为要使用 read(char[])方法，将读取到字符存入数组。所以要创建一个字符数组，一般数组的长度都是 1024 的整数倍。
+
+		char[] buf = new char[1024];
+		int len = 0;
+		while ((len = fr.read(buf)) != -1) {
+			System.out.println(new String(buf, 0, len));
+		}
+		fr.close();
+
+	}
+
+	/**
+	 * used: last update time : 2018年11月30日上午11:27:22 return :void
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void f2() throws IOException {
+		FileWriter fw = new FileWriter("bufdemo.txt");
+		BufferedWriter bufw = new BufferedWriter(fw);// 让缓冲区和指定流相关联。
+		for (int x = 0; x < 4; x++) {
+			bufw.write(x + "abc");
+			bufw.newLine(); // 写入一个换行符，这个换行符可以依据平台的不同写入不同的换行符。
+			bufw.flush();// 对缓冲区进行刷新，可以让数据到目的地中。
+		}
+		bufw.close();
+	}
+
+	/**
+	 * used: BufferedReader last update time : 2018年11月30日上午11:27:13 return :void
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void f3() throws IOException {
+		FileReader fr = new FileReader("bufdemo.txt");
+		BufferedReader bufr = new BufferedReader(fr);
+		String line = null;
+		while ((line = bufr.readLine()) != null) { // readLine 方法返回的时候是不带换行符的。
+			System.out.println(line);
+		}
+		bufr.close();
+	}
+	
+	/**
+	 * used: 获取键盘输入并打印出来
+	 * last update time : 2018年11月30日上午11:30:59
+	 * return :void
+	 * @throws IOException
+	 */
+	@Test
+	public void f4() throws IOException{
+		//记住，只要一读取键盘录入，就用这句话。
+		BufferedReader bufr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufw = new BufferedWriter(new OutputStreamWriter(System.out));//输出到控制台
+		String line = null;
+		while((line=bufr.readLine())!=null){
+		if("over".equals(line))
+		break;
+		bufw.write(line.toUpperCase());//将输入的字符转成大写字符输出
+		bufw.newLine();
+		bufw.flush();
+		}
+		bufw.close();
+		bufr.close();
+	}
+
+
+
+}
